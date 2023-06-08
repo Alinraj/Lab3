aea6568e2d373d0afd83bd705b39f793b254b834
Alinraj <134546581+Alinraj@users.noreply.github.com>
2023-06-09 04:18:51 +0530

    completed script

--- process_sales_data.py
+++ process_sales_data.py
@@ -25,7 +25,7 @@ def get_sales_csv_path():
     Returns:
         str: Path of sales data CSV file
     """
-    # Check whether command line parameter provided
+    # T Check whether command line parameter provided
     num_params = len(sys.argv) -1
     if num_params < 1:
         print("Error:Missing CSV path parameter.")
@@ -49,19 +49,19 @@ def create_orders_dir(sales_csv_path):
     Returns:
         str: Path of orders directory
     """
-    # Get directory in which sales data CSV file resides
+    #  Get directory in which sales data CSV file resides
     sales_csv_dir = os.path.dirname(sales_csv_path)
 
-    # Determine the path of the directory to hold the order data files
+
+    #  Determine the path of the directory to hold the order data files
     todays_date = date.today().isoformat()
     orders_dir = f'Orders_{todays_date}'
     orders_dir_path = os.path.join(sales_csv_dir,orders_dir)
 
-    # Create the orders directory if it does not already exist
+    #  Create the orders directory if it does not already exist
     if not os.path.isdir(orders_dir_path):
         os.makedirs(orders_dir_path)
-
-    # Return path of orders directory
+    #  Return path of orders directory
     return orders_dir_path
 
 def process_sales_data(sales_csv_path, orders_dir_path):
@@ -71,38 +71,59 @@ def process_sales_data(sales_csv_path, orders_dir_path):
         sales_csv_path (str): Path of sales data CSV file
         orders_dir_path (str): Path of orders directory
     """
-    # Import the sales data from the CSV file into a DataFrame
+    #  Import the sales data from the CSV file into a DataFrame
     df = pd.read_csv(sales_csv_path)
 
     #  Insert a new "TOTAL PRICE" column into the DataFrame
     df.insert(7,'TOTAL PRICE', df['ITEM QUANTITY'] * df['ITEM PRICE'])
-
-    #  Remove columns from the DataFrame that are not needed
+    # Remove columns from the DataFrame that are not needed
     df.drop(columns=['ADDRESS','CITY', 'STATE', 'POSTAL CODE', 'COUNTRY'], inplace=True)
-
-    # Groups orders by ID and iterate 
+    #  Groups orders by ID and iterate 
     for order_id, order_df in df.groupby('ORDER ID'):
-        # Remove the 'ORDER ID' column
-         order_df.drop(columns=['ORDER ID'], inplace=True)
+        #  Remove the 'ORDER ID' column
+        order_df.drop(columns=['ORDER ID'], inplace=True)
         # Sort the items by item number
-         order_df.sort_values(by='ITEM NUMBER', inplace=True)
+        order_df.sort_values(by='ITEM NUMBER', inplace=True)
         # Append a "GRAND TOTAL" row
         grand_total = order_df['TOTAL PRICE'].sum()
         grand_total_df = pd.DataFrame({'ITEM PRICE': ['GRAND TOTAL'], 'TOTAL PRICE': [grand_total]})
         order_df = pd.concat([order_df, grand_total_df])
-    
-        # Determine the file name and full path of the Excel sheet\
+        
+     
+        #  Determine the file name and full path of the Excel sheet
         customer_name = order_df['CUSTOMER NAME'].values[0]
         customer_name = re.sub(r'\w', '', customer_name)
         order_file = f'Order{order_id}_{customer_name}.xlsx'
         order_excel_path = os.path.join(orders_dir_path, order_file)
-        # Export the data to an Excel sheet
+       
+        #  Export the data to an Excel sheet
         worksheet_name = f'Order #{order_id}'
-        # Format the Excel sheet
-        
-        # TODO: Define format for the money columns
-        # TODO: Format each colunm
-        # TODO: Close the Excelwriter 
+        #order_df.to_excel(order_excel_path, index=False, sheet_name=worksheet_name)
+
+ # Format the Excel sheet
+
+        writer = pd.ExcelWriter("order_file.xlsx", engine="xlsxwriter")
+        order_df.to_excel(writer, index = False, sheet_name=worksheet_name)
+
+        workbook = writer.book
+        worksheet = writer.sheets[worksheet_name]
+
+ #  Define format for the money columns
+    cash_Format = workbook.add_format({"num_format" : "$#,##0.00"})
+   
+
+ #  Format each colunm
+    worksheet.set_column(0, 0, 11)
+    worksheet.set_column(1, 1, 13)
+    worksheet.set_column(2, 4, 15)
+    worksheet.set_column('F:G', 13, cash_Format)
+    worksheet.set_column('H:H', 10)
+    worksheet.set_column('I:I', 30)
+
+ #  Close the Excelwriter
+
+    writer.close()
+
     return
 
 if __name__ == '__main__':
